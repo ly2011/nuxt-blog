9d2779ef9df1cc2ec770caa5360e248a1c705a18
ly2011 <1518550424@qq.com>
2019-01-31 16:51:29 +0800

    <重构> 重构my-messages页面

--- pages/my/messages.vue
+++ pages/my/messages.vue
@@ -1,61 +1,101 @@
 <template>
-  <div class="msg-container">
-    <div class="hasnot_read_messages">
-      <el-card class="box-card">
-        <div slot="header" class="box-card-header">
-          <span>新消息</span>
+  <div class="msg-container container">
+    <div
+      id="main"
+      class="clearfix"
+    >
+      <side-bar />
+      <div id="content">
+        <div class="panel">
+          <header class="header">
+            <breadcrumb :breadcrumb="breadcrumb" />
+          </header>
+          <div class="inner">
+            <template v-if="messages.hasnot_read_messages.length > 0">
+              <div
+                v-for="msg in messages.has_read_messages"
+                :key="msg.id"
+                class="cell"
+              >
+                <span>
+                  <router-link
+                    :to="{ path: `/user/${msg.author.loginname}` }"
+                    target="_blank"
+                  >
+                    {{ msg.author.loginname }}
+                  </router-link>
+                  回复了你的话题
+                  <router-link
+                    :to="{ path: `/topic/${msg.id}` }"
+                    target="_blank"
+                  >
+                    {{ msg.topic.title }}
+                  </router-link>
+                </span>
+              </div>
+            </template>
+            <template v-else>
+              <p>无消息</p>
+            </template>
+          </div>
         </div>
-        <div class="box-card-content">
-          <template v-if="messages.hasnot_read_messages.length > 0">
-            <div v-for="msg in messages.has_read_messages" :key="msg.id" class="box-card-item">
-              <span>
-                <router-link :to="{}">
-                  {{ msg.author.loginname }}
-                </router-link>
-                回复了你的话题
-                <router-link :to="{name: 'topic', query: {id: msg.id}}">
-                  {{ msg.topic.title }}
-                </router-link>
-              </span>
-            </div>
-          </template>
-          <template v-else>
-            <p>无消息</p>
-          </template>
+
+        <div class="panel">
+          <header class="header">
+            <span class="col_fade">过往信息</span>
+          </header>
+          <div :class="{ inner: true, 'has-msg': messages.has_read_messages.length > 0 }">
+            <template v-if="messages.has_read_messages.length > 0">
+              <div
+                v-for="msg in messages.has_read_messages"
+                :key="msg.id"
+                class="cell"
+              >
+                <span>
+                  <router-link
+                    :to="{ path: `/user/${msg.author.loginname}` }"
+                    target="_blank"
+                  >
+                    {{ msg.author.loginname }}
+                  </router-link>
+                  回复了你的话题
+                  <!-- <router-link :to="{name: 'topic', query: {id: `${msg.topic.id}#${msg.reply.id}`}}"> -->
+                  <router-link
+                    :to="{ path: `/topic/${msg.id}` }"
+                    target="_blank"
+                  >
+                    {{ msg.topic.title }}
+                  </router-link>
+                </span>
+              </div>
+            </template>
+            <template v-else>
+              <p>无消息</p>
+            </template>
+          </div>
         </div>
-      </el-card>
-    </div>
-    <div class="has_read_messages">
-      <el-card class="box-card">
-        <div slot="header" class="box-card-header">
-          <span>过往信息</span>
-        </div>
-        <div class="box-card-content">
-          <template v-if="messages.has_read_messages.length > 0">
-            <div v-for="msg in messages.has_read_messages" :key="msg.id" class="box-card-item">
-              <span>
-                <router-link :to="{}">
-                  {{ msg.author.loginname }}
-                </router-link>
-                回复了你的话题
-                <router-link :to="{name: 'topic', query: {id: `${msg.topic.id}#${msg.reply.id}`}}">
-                  {{ msg.topic.title }}
-                </router-link>
-              </span>
-            </div>
-          </template>
-          <template v-else>
-            <p>无消息</p>
-          </template>
-        </div>
-      </el-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import SideBar from '~/components/SideBar';
+import Breadcrumb from '~/components/Breadcrumb';
 export default {
+  components: {
+    SideBar,
+    Breadcrumb
+  },
+  data () {
+    return {
+      breadcrumb: [
+        { title: '主页', active: false },
+        { title: '新消息', active: true }
+      ]
+    }
+  },
   computed: {
     // 从 store 的 state 对象中获取 topic
     ...mapGetters({
@@ -64,7 +104,7 @@ export default {
       loading: 'user/loading'
     })
   },
-  mounted() {
+  mounted () {
     const _self = this;
     setTimeout(() => {
       // 添加 persistedstate.js 中间件后，需要设置setTimout 0ms 后刷新页面才不会报错（accesstoken不会立即获取到值）
@@ -72,7 +112,7 @@ export default {
     }, 0);
   },
   methods: {
-    async fetchMessages() {
+    async fetchMessages () {
       try {
         const params = {
           accesstoken: this.accesstoken
@@ -97,38 +137,29 @@ export default {
   }
 };
 </script>
-
 <style lang="postcss" scoped>
-.msg-container {
-  .box-card {
-    margin: 0 5px;
-    border-radius: 4px 4px 0 0;
-    .box-card-item {
-      a {
-        color: #02b3ff;
-        max-width: 460px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: inline-block;
-        vertical-align: middle;
-        &:active,
-        &:hover {
-          color: #08c;
-          text-decoration: underline;
-        }
-      }
+.panel {
+  .header {
+    .col_fade {
+      color: #444;
     }
   }
-}
-</style>
+  p {
+    margin: 0 0 10px;
+  }
 
-<style lang="postcss">
-.msg-container {
-  .el-card.box-card {
-    border: none;
-    .el-card__header {
-      padding: 10px;
-      background-color: #f6f6f6;
+  .cell {
+    padding: 10px;
+    background-color: #fff;
+    border-top: 1px solid #f0f0f0;
+    a {
+      color: #08c;
+    }
+  }
+  .inner {
+    &.has-msg {
+      padding: 0;
+      border-radius: 0;
     }
   }
 }
